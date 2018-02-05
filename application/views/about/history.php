b83ed7cad37a26b09c460c7778015acd424e91d3
Thoritie <thohirh9@gmail.com>
2018-02-05 18:32:21 +0700

    Home :smile:

---
+++ application/views/about/history.php
@@ -0,0 +1,111 @@
+<section class="banner_area">
+   <div class="container">
+      <div class="banner_text_inner">
+         <h4>History of walailak University</h4>
+         <ul>
+            <li><a href="index.html"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li>
+            <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>About</a></li>
+            <li><a href="static.html"><i class="fa fa-angle-right" aria-hidden="true"></i>History</a></li>
+         </ul>
+      </div>
+   </div>
+</section>
+<div class="container">
+   <!--================Static Area =================-->
+   <section class="static_area">
+      <div class="container">
+         <div class="static_inner">
+            <div class="row">
+               <div class="col-lg-9">
+                  <div class="static_main_content">
+                     <div class="static_social">
+                        <ul>
+                           <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                           <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                           <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                           <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                           <li><a href="#"><i class="fa fa-thumb-tack" aria-hidden="true"></i></a></li>
+                        </ul>
+                     </div>
+                     <div class="static_img">
+                        <img class="img-fluid" src="img/static-1.jpg" alt="">
+                        <h4>History</h4>
+                     </div>
+                     <div class="static_text">
+                        <p>Walailak University is designed as a comprehensive university (with a variety of fields of study) to serve the people of Thailand.</p>
+                        <p>Walailak University graduates are expected to have practical intelligence, adversary quotient, Liberal Arts perspective and technology competence, all integrated with high moral. All activities are designed to enhance Walailak University’s uniqueness in its ultimate goal to become</p>
+                     </div>
+                     <div class="media">
+                        <div class="d-flex">
+                           <h6>“</h6>
+                        </div>
+                        <div class="media-body">
+                           <p>“The Education Park of ASEAN”</p>
+                           <h5><a href="#">here at</a> - Walailak University</h5>
+                        </div>
+                     </div>
+                     <div class="static_text2">
+                        <h3>Unique University</h3>
+                        <p>where all the global citizens can find the riches in the knowledge, wisdom and culture. </p>
+                        <p> Walailak University is a residential university equipped with modern educational facilities, services and accommodation for students and staff, located against a beautiful backdrop of mountain scenery in Tha Sala District, Nakhon Si Thammarat Province, Thailand. The campus is 780 kilometers south of Bangkok, a 21⁄2 hour drive from the beach resorts of Samui Island, Surat Thani and Krabi, and from the busy commercial center and international airport of Hat Yai. Nakhon Si Thammarat is well served by daily air, train and bus services from Bangkok, Hat Yai, Phuket, and Malaysia. The University has a total area of 3600 acres, making it the largest campus of any university in Thailand. Besides the main campus at Tha Sala, Walailak also has a coordinating office in Bangkok and a service center in neighboring Surat Thani province.</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-3">
+                  <div class="right_sidebar_area">
+                     <aside class="right_widget r_news_widget">
+                        <div class="r_w_title">
+                           <h3>Recent News</h3>
+                        </div>
+                        <div class="news_inner">
+                           <div class="news_item">
+                              <a href="#">
+                                 <h4>In order to succeed, we must first believe that we can.</h4>
+                              </a>
+                              <a href="#">
+                                 <h6>October 7, 2017</h6>
+                              </a>
+                           </div>
+                           <div class="news_item">
+                              <a href="#">
+                                 <h4>The way to get started is to quit talking and begin doing.</h4>
+                              </a>
+                              <a href="#">
+                                 <h6>October 7, 2017</h6>
+                              </a>
+                           </div>
+                           <div class="news_item">
+                              <a href="#">
+                                 <h4>In order to succeed, we must first believe that we can.</h4>
+                              </a>
+                              <a href="#">
+                                 <h6>October 7, 2017</h6>
+                              </a>
+                           </div>
+                           <div class="news_item">
+                              <a href="#">
+                                 <h4>The way to get started is to quit talking and begin doing.</h4>
+                              </a>
+                              <a href="#">
+                                 <h6>October 7, 2017</h6>
+                              </a>
+                           </div>
+                        </div>
+                     </aside>
+                     <aside class="right_widget r_cat_widget">
+                        <div class="r_w_title">
+                           <h3>Categories</h3>
+                        </div>
+                        <ul>
+                           <li><a href="#">Activities</a></li>
+                           <li><a href="#">News</a></li>
+                        </ul>
+                     </aside>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!--================End Static Area =================-->
+</div>
