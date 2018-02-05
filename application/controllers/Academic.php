053ea0b11b47241a7938a12644640efa9bbb49b8
Thoritie <thohirh9@gmail.com>
2018-02-05 22:47:52 +0700

    new controller:smile:

---
+++ application/controllers/Academic.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Academic extends CI_Controller {
+
+
+
+	public function digital(){
+
+	}
+
+	public function logistic(){
+		
+	}
+
+	public function publicAffairs(){
+		
+	}
+
+
+	
+
+	
+}
