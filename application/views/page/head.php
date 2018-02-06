ba005aa6fdee29a21985e56b8c91364c1c3a3592
Tannnnn <tan_sweetmulet@hotmail.com>
2018-02-06 15:48:26 +0700

    add sub dropdown

--- application/views/page/head.php
+++ application/views/page/head.php
@@ -28,6 +28,8 @@
 
         <link href="<?= base_url(); ?>public/css/style.css" rel="stylesheet">
         <link href="<?= base_url(); ?>public/css/responsive.css" rel="stylesheet">
+        <link href="<?= base_url(); ?>public/css/sub-dropdown.css" rel="stylesheet">
+        
        
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
