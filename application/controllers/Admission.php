797fea7c5a710e5d43cfc8afc2fd6c77b95ef1a7
lifnaja <alif23669@gmail.com>
2018-02-06 23:02:12 +0700

    applicantQualification page

--- application/controllers/Admission.php
+++ application/controllers/Admission.php
@@ -3,4 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admission extends CI_Controller {
 
+    public function applicantQualification(){
+		$this->load->helper('url');
+		
+		$data['breadcrumb'] = array(
+		    array("page"=>"Home", "url"=>base_url(), "icon"=>"fa fa-home"),
+		    array("page"=>"About", "url"=>"#", "icon"=>"fa fa-angle-right"),
+		    array("page"=>"Applicant Qualification", "url"=>base_url()."main/applicantQualification", "icon"=>"fa fa-angle-right")
+		);
+		
+		$this->load->view('page/head');
+		$this->load->view('page/menu');
+		$this->load->view('page/header_page', $data);
+		$this->load->view('Admission/ApplicantQualification');
+		$this->load->view('page/news');
+		$this->load->view('page/footer');
+		$this->load->view('page/foot');
+	}
 }
