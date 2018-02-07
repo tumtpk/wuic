f5bcb5947b5e9e4cc862f6520b0cc863a0c665b5
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2018-02-07 16:19:38 +0700

    add page digital

--- application/views/academic/digital.php
+++ application/views/academic/digital.php
@@ -14,7 +14,22 @@
                      </div>
                      <div class="static_img">
                         <img class="img-fluid" src="img/static-1.jpg" alt="">
-                        <h4>Digital</h4>
+                        <h4>Program in Digital Innovation Engineering</h4><br>
+                        <p>This program aims to produce graduates who inspire to change the digital world through innovative designs of applications and how they interact with humans worldwide. Graduates will respond to problems while creating new opportunities from digital innovations.</p><br>
+                        <div class="form-group row text-center">
+                           	<div class="col-md">
+                              	<img src="<?=base_url() ?>public/img/page/xIN-300x141.png.pagespeed.ic._qxOQntmBE.webp" class="img-fluid" alt="Responsive image"><br>
+                            </div>
+                      	</div>
+                        <p>Graduates can pursue work in areas such as:</p><br>
+                        <p>1. Computer Engineering</p>
+                        <p>2. Innovative Engineers</p>
+                        <p>3. Problem-solver in creative innovations</p>
+                        <p>4. Software developers or programmer in an organization</p>
+                        <p>5. Entrepreneurs in the computer industry or a founder of a startup</p>
+                        <p>6. Digital products designers</p>
+                        <p>7. Academic, Researcher, Technician in the field of digital innovation</p>
+
                      </div>
                      <div class="static_text">
                         
