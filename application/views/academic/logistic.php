95b74a2fa7e32b3cb70f42a3b0769e9a5366aaca
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2018-02-07 17:16:19 +0700

    add page logistic

---
+++ application/views/academic/logistic.php
@@ -0,0 +1,50 @@
+<div class="container">
+   <!--================Static Area =================-->
+   <section class="static_area">
+      <div class="container">
+         <div class="static_inner">
+            <div class="row">
+               <div class="col-lg-9">
+                  <div class="static_main_content">
+                     <div class="static_social">
+                        <ul>
+                           <li><a href="http://www.facebook.com/sharer.php?u=<?=current_url()?>"><i class="fa fa-facebook"></i></a></li>
+                           <li><a href="https://twitter.com/intent/tweet?text=Program+in+Logistics+Analytics+and+Supply+Chain+Management&url=<?=current_url()?>"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                     </div>
+                     <div class="static_img">
+                        <img class="img-fluid" src="img/static-1.jpg" alt="">
+                        <h4>Program in Logistics Analytics and Supply Chain Management</h4><br>
+                     </div>
+                     <div class="static_text2">
+                        <div class="form-group row text-center">
+                           	<div class="col-md">
+                              	<img src="<?=base_url() ?>public/img/page/LA-300x148.png" class="img-fluid" alt="Responsive image"><br>
+                            </div>
+                      	</div>
+                        <p>The program emphasizes graduates with a basic knowledge of analytics, planning and best practice of management. Students are trained to use decision making processes that utilize information to serve the needs of logistics and supply chains of local and international markets. Awareness of change in the logistic and business environment, adept at skills used, active learning. Ethical and social responsibility are simultaneously taught throughout the courses.</p><br>
+                        <p>Graduates can pursue work in areas such as:</p><br>
+                        <h6>1. Analyzer, planner and designer in Logistics </h6>
+                        <p>1. Supply chain planner and designer</p>
+                        <p>2. Production Planner</p>
+                        <p>3. Business development officer</p>
+                        <p>4. Logistics and supply chain analyst</p>
+                        <p>5. Business information system developer</p>
+                        <p>6. Business Process Analyst</p><br>
+                        <h6>2. Officer or chief executive in the operations group.</h6>
+                        <p>1. Supply Chain and Logistics functions</p>
+                        <p>2. Purchasing functions</p>
+                        <p>3. Import and export functions</p>
+                        <p>4. Warehouse functions</p>
+                        <p>5. Operation functions</p>
+                        <p>6. Transportation functions</p>
+                        <p>7. Production functions</p>
+                        <p>8. Port functions</p><br>
+                        <h6>3. Entrepreneurship</h6>
+                        <p>1. Import and export company</p>
+                        <p>2. Logistics provider</p>
+                        <p>3. Transportation Agent / Logistics service provider</p>
+                    </div>
+                  </div>
+               </div>
+               
