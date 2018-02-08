e3f45a9d47ff12d9639d71f58eb02654b0560e80
TumPK <SWE-PC@172.16.83.112>
2018-02-08 10:06:46 +0700

    change form logistic

--- application/views/academic/logistic.php
+++ application/views/academic/logistic.php
@@ -25,25 +25,31 @@
                         <p>The program emphasizes graduates with a basic knowledge of analytics, planning and best practice of management. Students are trained to use decision making processes that utilize information to serve the needs of logistics and supply chains of local and international markets. Awareness of change in the logistic and business environment, adept at skills used, active learning. Ethical and social responsibility are simultaneously taught throughout the courses.</p><br>
                         <p>Graduates can pursue work in areas such as:</p><br>
                         <h6>1. Analyzer, planner and designer in Logistics </h6>
-                        <p>1. Supply chain planner and designer</p>
-                        <p>2. Production Planner</p>
-                        <p>3. Business development officer</p>
-                        <p>4. Logistics and supply chain analyst</p>
-                        <p>5. Business information system developer</p>
-                        <p>6. Business Process Analyst</p><br>
+                        <div class="col-md">
+                            <p>1. Supply chain planner and designer</p>
+                            <p>2. Production Planner</p>
+                            <p>3. Business development officer</p>
+                            <p>4. Logistics and supply chain analyst</p>
+                            <p>5. Business information system developer</p>
+                            <p>6. Business Process Analyst</p><br>
+                        </div>
                         <h6>2. Officer or chief executive in the operations group.</h6>
-                        <p>1. Supply Chain and Logistics functions</p>
-                        <p>2. Purchasing functions</p>
-                        <p>3. Import and export functions</p>
-                        <p>4. Warehouse functions</p>
-                        <p>5. Operation functions</p>
-                        <p>6. Transportation functions</p>
-                        <p>7. Production functions</p>
-                        <p>8. Port functions</p><br>
+                        <div class="col-md">
+                            <p>1. Supply Chain and Logistics functions</p>
+                            <p>2. Purchasing functions</p>
+                            <p>3. Import and export functions</p>
+                            <p>4. Warehouse functions</p>
+                            <p>5. Operation functions</p>
+                            <p>6. Transportation functions</p>
+                            <p>7. Production functions</p>
+                            <p>8. Port functions</p><br>
+                        </div>
                         <h6>3. Entrepreneurship</h6>
-                        <p>1. Import and export company</p>
-                        <p>2. Logistics provider</p>
-                        <p>3. Transportation Agent / Logistics service provider</p>
+                        <div class="col-md">
+                            <p>1. Import and export company</p>
+                            <p>2. Logistics provider</p>
+                            <p>3. Transportation Agent / Logistics service provider</p>
+                        </div>
                     </div>
                   </div>
                </div>
