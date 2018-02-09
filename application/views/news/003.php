bd42d048a63a4f10c4db2d870513f16874f83b4f
TumPK <SWE-PC@172.16.83.167>
2018-02-09 14:02:25 +0700

    add News

---
+++ application/views/news/003.php
@@ -0,0 +1,44 @@
+<div class="container">
+   <!--================Static Area =================-->
+   <section class="static_area">
+      <div class="container">
+         <div class="static_inner">
+            <div class="row">
+               <div class="col-lg-9">
+                  <div class="static_main_content">
+                     <div class="static_social">
+                        <ul>
+                           <li><a href="http://www.facebook.com/sharer.php?u=<?=current_url()?>"><i class="fa fa-facebook"></i></a></li>
+                           <li><a href="https://twitter.com/intent/tweet?text=Program+in+Logistics+Analytics+and+Supply+Chain+Management&url=<?=current_url()?>"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                     </div>
+                     <div class="static_img">
+                        <img class="img-fluid" src="img/static-1.jpg" alt="">
+                        <h4><?=$title ?></h4>
+						<div class="news-date">
+                     		<i class="fa fa-clock-o"></i> <?=$date ?>
+                     	</div>                     
+                     </div>
+                     <div class="static_text2">
+                        <p>This program aims to produce graduates with a globalization mindset. Able to quickly respond to the global changes consciously and through positive thinking. Commitment to self-development and transformational leadership through creative innovation in order to provide a competitive advantage in public affairs.</p>
+                     	
+                     	<p>Majors</p>
+                     	<p>1. Public Policy and Global Affairs</p>
+                     	<p>2. Public Governance</p>
+                     	
+                     	<p>Graduates can pursue work in areas such as:</p>
+                     	<div class="col-md">
+                         	<p>1. Administrative officers</p>
+                         	<p>2. Local administrative officers</p>
+                         	<p>3. Politicians</p>
+                         	<p>4. Independent organization officers</p>
+                         	<p>5. Local and international private organization officers</p>
+                         	<p>6. Research and development Projects officers</p>
+                         	<p>7. Diplomat</p>
+                         	<p>8. International organization or overseas officers</p>
+                         	<p>9. Academic officers</p>
+                     	</div>
+                     </div>
+                  </div>
+               </div>
+               
---
+++ application/views/news/004.php
@@ -0,0 +1,38 @@
+<div class="container">
+   <!--================Static Area =================-->
+   <section class="static_area">
+      <div class="container">
+         <div class="static_inner">
+            <div class="row">
+               <div class="col-lg-9">
+                  <div class="static_main_content">
+                     <div class="static_social">
+                        <ul>
+                           <li><a href="http://www.facebook.com/sharer.php?u=<?=current_url()?>"><i class="fa fa-facebook"></i></a></li>
+                           <li><a href="https://twitter.com/intent/tweet?text=Program+in+Logistics+Analytics+and+Supply+Chain+Management&url=<?=current_url()?>"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                     </div>
+                     <div class="static_img">
+                        <img class="img-fluid" src="img/static-1.jpg" alt="">
+                        <h4><?=$title ?></h4>
+						<div class="news-date">
+                     		<i class="fa fa-clock-o"></i> <?=$date ?>
+                     	</div>                     
+                     </div>
+                     <div class="static_text2">
+                        <p>This program aims to produce graduates who inspire to change the digital world through innovative designs of applications and how they interact with humans worldwide. Graduates will respond to problems while creating new opportunities from digital innovations.</p>
+                     	
+                     	<p>Graduates can pursue work in areas such as:</p>
+                     	<div class="col-md">
+                         	<p>1. Computer Engineering</p>
+                         	<p>2. Innovative Engineers</p>
+                         	<p>3. Problem-solver in creative innovations</p>
+                         	<p>4. Software developers or programmer in an organization</p>
+                         	<p>5. Entrepreneurs in the computer industry or a founder of a startup</p>
+                         	<p>6. Digital products designers</p>
+                         	<p>7. Academic, Researcher, Technician in the field of digital innovation</p>
+                     	</div>
+                     </div>
+                  </div>
+               </div>
+               
