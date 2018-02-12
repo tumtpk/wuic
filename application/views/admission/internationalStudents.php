3a3fa1c0139b2d0c09452e4274fefd9a86d895e0
lifnaja <alif23669@gmail.com>
2018-02-12 13:54:32 +0700

    internationalStudents page

--- application/views/admission/internationalStudents.php
+++ application/views/admission/internationalStudents.php
@@ -1,1 +1,270 @@
-International Students and Non TCAS system’s applicants
+<div class="container">
+   <!--================Static Area =================-->
+   <section class="static_area">
+      <div class="container">
+         <div class="static_inner">
+            <div class="row">
+               <div class="col-lg-9">
+                  <div class="static_main_content">
+                     <div class="static_social">
+                        <ul>
+                           <li><a href="http://www.facebook.com/sharer.php?u=<?=current_url()?>"><i class="fa fa-facebook"></i></a></li>
+                           <li><a href="https://twitter.com/intent/tweet?text=Program+in+Logistics+Analytics+and+Supply+Chain+Management&url=<?=current_url()?>"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                     </div>
+                     <div class="static_img">
+                        <img class="img-fluid" src="img/static-1.jpg" alt="">
+                        <h4>International Students and Non TCAS system’s applicants</h4>
+                     </div>
+                     <div class="static_text2">
+                        <p>1.<u>Admission Criteria</u></p>
+                        <p>Please select and submit 1 of standardized test score with minimum required as presented below.</p>
+                        <div class="table-responsive table-bordered"  border-style: solid;border-color: coral;>
+                            <table class="table">
+                                    <tr class="">
+                                        <td style="text-align: center;"><b>Type of Score</b></td>
+                                        <td style="text-align: center;"><strong>Subjects</strong></td>
+                                        <td style="text-align: center;"><strong>Digital Innovation Engineering</strong></td>
+                                        <td style="text-align: center;"><strong>Public Affairs</strong></td>
+                                        <td style="text-align: center;"><strong>Logistic Analytics and Supply Chain Management </strong></td>
+                                    </tr>
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="3"> <strong>SAT I</strong></td>
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;">600</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">600</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Critical Reading</td>
+                                        <td style="text-align: center;">350</td>
+                                        <td style="text-align: center;">350</td>
+                                        <td style="text-align: center;">350</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Writing </td>
+                                        <td style="text-align: center;">350</td>
+                                        <td style="text-align: center;">350</td>
+                                        <td style="text-align: center;">350</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="2"> <strong>New SAT I</strong></td>
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;">620</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">620</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Evidence-Based Reading and Writing</td>
+                                        <td style="text-align: center;">350</td>
+                                        <td style="text-align: center;">350</td>
+                                        <td style="text-align: center;">350</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="2"> <strong>SAT II</strong></td>
+                                        <td>Mathematics (Level 2)</td>
+                                        <td style="text-align: center;">600</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">600</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Physics</td>
+                                        <td style="text-align: center;">600</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="4"> <strong>ACT</strong></td>
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;vertical-align: middle;" rowspan="4">Composite Score 21.5 from 36 (Math ≥ 20 and Science ≥ 20)</td>
+                                        <td style="text-align: center;vertical-align: middle;" rowspan="4">Composite Score 21.5 from 36 (English ≥ 20 and Reading ≥ 20)</td>
+                                        <td style="text-align: center;vertical-align: middle;" rowspan="4">Composite Score 21.5 from 36 (Math ≥ 20 and English ≥ 20)</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Science</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>English</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Reading</td>
+                                    </tr>
+
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="5"> <strong>GED</strong></td>
+                                        <td>Reasoning Through Language Arts</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">160</td>
+                                        <td style="text-align: center;">160</td>
+                                       
+                                    </tr>
+                                    <tr class="">
+                                        <td>Mathematic Reasoning</td>
+                                        <td style="text-align: center;">160</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">160</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Science</td>
+                                        <td style="text-align: center;">160</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Social Studies</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">160</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" colspan="4">Composite Score at least 580 from 800</td>
+                                    </tr>
+
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="2"> <strong>IB (Higher or Standard Level)</strong></td>
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;">≥ 4</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">≥ 4</td>
+                                       
+                                    </tr>
+                                    <tr class="">
+                                        <td>Physics</td>
+                                        <td style="text-align: center;">≥ 4</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+
+                                     <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="8"> <strong>GCSE, IGCSE, or ‘O’ Level ( 5 Subjects) and GCE ‘AS’, or ‘A’ level(3 Subjects)</strong></td>
+                                        <td   style="text-align: center; vertical-align: middle;" colspan="4">Subjects of GCSE, IGCSE, or ‘O’ Level</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;">B</td>
+                                        <td style="text-align: center;">C</td>
+                                        <td style="text-align: center;">B</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Physics or Co. ,Sci.</td>
+                                        <td style="text-align: center;">B</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Any 3 other subjects</td>
+                                        <td style="text-align: center;" colspan="3">C</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" colspan="4">Subjects of GCSE, IGCSE, or ‘A’ Level</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;" colspan="3">C</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Physics</td>
+                                        <td style="text-align: center;">C</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Any ‘AS’ or ‘A’ level subjects</td>
+                                        <td style="text-align: center;vertical-align: middle;" colspan="3">C</td>
+                                    </tr>
+
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="3"> <strong>Sixth Form(5 Subjects)</strong></td>
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;" colspan="3">≥ 4</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Physics</td>
+                                        <td style="text-align: center;">≥ 4</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Any 3 other subjects</td>
+                                        <td style="text-align: center;vertical-align: middle;" colspan="3">≥ 5</td>
+                                    </tr>
+                                    
+                                    <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="10"> <strong>CEA Level 2-3(5 Subjects)</strong></td>
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;" colspan="3">2.5</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Physics or Science</td>
+                                        <td style="text-align: center;">2.5</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Any 3 other subjects</td>
+                                        <td style="text-align: center;vertical-align: middle;" colspan="3">2</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;vertical-align: middle;" colspan="4">How to calculate NCEA to GPA (min. 7 credits/subject)</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;vertical-align: middle;" colspan="4"> – Grade E (Excellence) = 4</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;vertical-align: middle;" colspan="4"> – Grade M (Merit) = 3</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;vertical-align: middle;" colspan="4"> – Grade A (Achieved) = 2</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;vertical-align: middle;" colspan="4">– Grade N (Not Achieved) = 0</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;vertical-align: middle;" colspan="4">– Multiply the value of each grade by the number of credits the course worth.</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;vertical-align: middle;" colspan="4">– Divide the total value of all the grades you have received by the total number of credits you have completed.</td>
+                                    </tr>
+
+                                     <tr class="">
+                                        <td   style="text-align: center; vertical-align: middle;" rowspan="9"> <strong>BHSEC(Bhutan)(5 Subjects)</strong></td>
+                                        <td>Mathematics</td>
+                                        <td style="text-align: center;" colspan="3">B</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Physics or Science</td>
+                                        <td style="text-align: center;">B</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Physics or Science</td>
+                                        <td style="text-align: center;">B</td>
+                                        <td style="text-align: center;">-</td>
+                                        <td style="text-align: center;">-</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>Any 3 other subjects</td>
+                                        <td style="text-align: center;" colspan="3">C</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;" colspan="4">Grade 1 = A*</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;" colspan="4">Grade 2 = A</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;" colspan="4">Grade 3-5 = B</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;" colspan="4">Grade 6-7 = C</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td style="text-align: center;" colspan="4">Grade 8-9 = F</td>
+                                    </tr>
+                            </table>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               
