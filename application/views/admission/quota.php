beb109ae8acc20f45eaa4e81ed5aa605cb0fe908
lifnaja <alif23669@gmail.com>
2018-02-12 12:56:37 +0700

    page quoto

---
+++ application/views/admission/quota.php
@@ -0,0 +1,71 @@
+<div class="container">
+   <!--================Static Area =================-->
+   <section class="static_area">
+      <div class="container">
+         <div class="static_inner">
+            <div class="row">
+               <div class="col-lg-9">
+                  <div class="static_main_content">
+                     <div class="static_social">
+                        <ul>
+                           <li><a href="http://www.facebook.com/sharer.php?u=<?=current_url()?>"><i class="fa fa-facebook"></i></a></li>
+                           <li><a href="https://twitter.com/intent/tweet?text=Program+in+Logistics+Analytics+and+Supply+Chain+Management&url=<?=current_url()?>"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                     </div>
+                     <div class="static_img">
+                        <img class="img-fluid" src="img/static-1.jpg" alt="">
+                        <h4>Quota</h4>
+                     </div>
+                     <div class="static_text2">
+                        <p>การรับสมัครนักศึกษา ประเภทรับแบบโควต้า 14 จังหวัดภาคใต้ รวมจังหวัดประจวบคีรีขันธ์ ประจำปีการศึกษา 2561</p>
+                        <p><u>เกณฑ์การรับสมัคร</u></p>
+                        <p>การรับสมัคร จะพิจารณาจากผลการเรียนเฉลี่ยสะสม จำนวนหน่วยกิต และแฟ้มสะสมผลงาน แยกตามหลักสูตรที่เปิดรับสมัคร ดังนี้</p>
+                        <div class="table-responsive table-bordered"  border-style: solid;border-color: coral;>
+                            <table class="table">
+                                    <tr class="">
+                                        <td rowspan="2">หลักสูตร</td>
+                                        <td rowspan="2">แผนการเรียน</td>
+                                        <td colspan="4"  style="text-align: center;">เกณฑ์การรับและสัดส่วนของแต่ละรายวิชาที่นำมาใช้ในการคัดเลือก (%)</td>
+                                        <td rowspan="2">คะแนนการสอบภาษาอังกฤษ (IELT)*</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>GAT1</td>
+                                        <td>GAT2</td>
+                                        <td>PAT1</td>
+                                        <td>9 วิชาสามัญ</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>สาขาวิศวกรรมนวัตกรรมดิจิทัล</td>
+                                        <td>วิทย์-คณิต</td>
+                                        <td>-</td>
+                                        <td>30%</td>
+                                        <td>40%</td>
+                                        <td>ฟิสิกส์ 30%</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>สาขากิจการสาธารณะ</td>
+                                        <td>รับทุกแผน</td>
+                                        <td>40%</td>
+                                        <td>30%</td>
+                                        <td>-</td>
+                                        <td>สังคม 30%</td>
+                                        <td>6.0</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>สาขาการวิเคราะห์โลจิสติกส์และการจัดการซัพพลายเชน</td>
+                                        <td>วิทย์-คณิต หรือศิลป์-คำนวณ</td>
+                                        <td>30%</td>
+                                        <td>40%</td>
+                                        <td>30%</td>
+                                        <td>-</td>
+                                        <td></td>
+                                    </tr>
+                            </table>
+                        </div>
+                        <p><strong>หมายเหตุ</strong></p>
+                        <p>* คะแนนการสอบภาษาอังกฤษ IELT 6.0 หรือคะแนนการสอบภาษาอังกฤษอื่นๆ ที่เทียบเท่า กรณีไม่มีคะแนนการสอบภาษาอังกฤษสามารถใช้ผลการศึกษาภาษาอังกฤษในระดับมัธยมปลายเฉลี่ย ตั้งแต่ 3.5 ขึ้นไปแทนได้</p>
+                     </div>
+                  </div>
+               </div>
+               
