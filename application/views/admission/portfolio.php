82a41dfb2bf999cfa5d7abb19b200cd82efd9cfa
lifnaja <alif23669@gmail.com>
2018-02-12 12:56:55 +0700

    page portfolio

---
+++ application/views/admission/portfolio.php
@@ -0,0 +1,78 @@
+<div class="container">
+   <!--================Static Area =================-->
+   <section class="static_area">
+      <div class="container">
+         <div class="static_inner">
+            <div class="row">
+               <div class="col-lg-9">
+                  <div class="static_main_content">
+                     <div class="static_social">
+                        <ul>
+                           <li><a href="http://www.facebook.com/sharer.php?u=<?=current_url()?>"><i class="fa fa-facebook"></i></a></li>
+                           <li><a href="https://twitter.com/intent/tweet?text=Program+in+Logistics+Analytics+and+Supply+Chain+Management&url=<?=current_url()?>"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                     </div>
+                     <div class="static_img">
+                        <img class="img-fluid" src="img/static-1.jpg" alt="">
+                        <h4>Portfolio</h4>
+                     </div>
+                     <div class="static_text2">
+                        <p>การรับสมัครนักศึกษา ประเภทรับตรงทั่วประเทศด้วย Portfolio ประจำปีการศึกษา 2561</p>
+                        <p><u>เกณฑ์การรับสมัคร</u></p>
+                        <p>การรับสมัคร จะพิจารณาจากผลการเรียนเฉลี่ยสะสม จำนวนหน่วยกิต และแฟ้มสะสมผลงาน แยกตามหลักสูตรที่เปิดรับสมัคร ดังนี้</p>
+                        <div class="table-responsive table-bordered"  border-style: solid;border-color: coral;>
+                            <table class="table">
+                                    <tr class="">
+                                        <td rowspan="2">หลักสูตร</td>
+                                        <td rowspan="2">จำนวนรับ</td>
+                                        <td colspan="5"  style="text-align: center;">เกณฑ์การรับสมัคร</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>ผลการเรียนเฉลี่ยสะสม*</td>
+                                        <td>จำนวนหน่วยกิตกลุ่มวิทยาศาสตร์</td>
+                                        <td>จำนวนหน่วยกิต กลุ่มคณิตศาสตร์</td>
+                                        <td>แฟ้มสะสม ผลงาน (Portfolio)**</td>
+                                        <td>คะแนนการสอบภาษาอังกฤษ (IELT)***</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>สาขาวิศวกรรมนวัตกรรมดิจิทัล</td>
+                                        <td>30</td>
+                                        <td>≥ 2.75</td>
+                                        <td>≥ 15 หน่วยกิต</td>
+                                        <td>≥ 8 หน่วยกิต</td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>สาขากิจการสาธารณะ</td>
+                                        <td>40</td>
+                                        <td>≥ 2.75	</td>
+                                        <td>ไม่กำหนด</td>
+                                        <td>ไม่กำหนด</td>
+                                        <td>√</td>
+                                        <td>6.0</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td>สาขาการวิเคราะห์โลจิสติกส์และการจัดการซัพพลายเชน</td>
+                                        <td>30</td>
+                                        <td>≥ 2.75	</td>
+                                        <td>ไม่กำหนด</td>
+                                        <td>≥ 8 หน่วยกิต</td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                            </table>
+                        </div>
+                        <p><strong>หมายเหตุ</strong></p>
+                        <p>* ผู้สมัครจะต้องมีผลการเรียนเฉลี่ยสะสมชั้นมัธยมปลาย 5 ภาคเรียน (ยกเว้นกรณีที่ผลการเรียนชั้นมัธยมศึกษาปีที่ 6 ภาคเรียนที่ 1 ยังไม่ประกาศผล สามารถยื่น 4 ภาคเรียนได้ โดยให้ยื่นคะแนนภาคเรียนที่ 5 เมื่อมีการประกาศผลคะแนนจากโรงเรียน)</p>
+                        <p>* กรณีผู้สมัครมีผลการเรียนในระบบเกรดแตกต่างจากระบบการศึกษาไทย ใช้การเทียบเคียง สูงกว่าระดับกลางของระบบนั้นๆ</p>
+                        <p>** แฟ้มสะสมผลงาน (Portfolio) ประกอบด้วย</p>
+                        <div class="col">
+                            <p> <i class="fa fa-circle-thin"></i> ประวัติ/ ข้อมูลส่วนตัว</p>
+                            <p> <i class="fa fa-circle-thin"></i> เอกสารประกอบแสดงผลงาน หรือกิจกรรมทั้งด้านวิชาการและด้านสังคม รวมทั้งเกียรติบัตร หรือ ประกาศนียบัตร รับรองด้านคุณธรรม จริยธรรม หรือจิตสาธารณะ พร้อมระบุผลที่ได้จากการดำเนินกิจกรรม</p>
+                        </div>
+                        <p>*** คะแนนการสอบภาษาอังกฤษ IELT 6.0 หรือคะแนนการสอบภาษาอังกฤษอื่นๆ ที่เทียบเท่า กรณีไม่มีคะแนนการสอบภาษาอังกฤษสามารถใช้ผลการศึกษาภาษาอังกฤษในระดับมัธยมปลายเฉลี่ย ตั้งแต่ 3.5 ขึ้นไปแทนได้</p>
+                     </div>
+                  </div>
+               </div>
+               
