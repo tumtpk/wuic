87e2f1a3594b9bbc6f4ccf5af6bbdc524520f039
Thoritie <thohirh9@gmail.com>
2018-02-13 16:19:41 +0700

    :beer:

--- application/views/contact/index.php
+++ application/views/contact/index.php
@@ -4,27 +4,79 @@
 <div class="container">
 <div class="static_inner">
 <div class="row">
-<div class="col-lg-9">
+
+<!-- this is section for new -->
+<!-- <div class="col-lg-9">
    <div class="static_main_content">
-      <div class="contact_text">
-         <div class="main_title">
-            <h2>Contact Us</h2>
-            <p>Walailak University International College (WUIC)</p>
-         </div>
-         <div class="contact_d_list">
-            <div class="contact_d_list_item">
-               <a href="#">+66 75 672 426-7</a>
-               <a href="#">+66 81 979 9651</a>
-               <a href="#">+66 96 998 3944</a>
-               <a href="#">wuic@wu.ac.th</a>
+     
+   </div>
+</div> -->
+
+<!-- end this is section for new -->
+<section class="contact_us_area">
+   <div class="container">
+     
+      <div class="contact_details_inner">
+         <div class="row">
+            <div class="col-lg-6">
+               <div class="contact_text">
+                  <div class="main_title">
+                     <h2>Contact Us</h2>
+                     <p>Walailak University International College (WUIC)</p>
+                  </div>
+                  <div class="contact_d_list">
+                     <div class="contact_d_list_item">
+                        <a href="#">+66 75 672 426-7</a>
+                        <a href="#">+66 81 979 9651</a>
+                        <a href="#">+66 96 998 3944</a>
+                        <a href="#">wuic@wu.ac.th</a>
+                     </div>
+                     <div class="contact_d_list_item">
+                        <p>Walailak University 222 Thaiburi, Thasala District,<br /> Nakhon Si Thammarat 80161, Thailand</p>
+                     </div>
+                     <!-- <div class="contact_d_list_item">
+                        <p>Open hours: 8.00-18.00 Mon-Fri <br />Sunday: Closed</p>
+                        </div> -->
+                  </div>
+                  <div class="static_social">
+                     <div class="main_title">
+                        <h2>Follow Us:</h2>
+                     </div>
+                     <ul>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                     </ul>
+                  </div>
+               </div>
             </div>
-            <div class="contact_d_list_item">
-               <p>Walailak University 222 Thaiburi, Thasala District,<br /> Nakhon Si Thammarat 80161, Thailand</p>
+            <div class="col-lg-6">
+               <div class="contact_form">
+                  <div class="main_title">
+                     <h2>Get In Touch With Us!</h2>
+                     <p>Fill out the form below to recieve a free and confidential.</p>
+                  </div>
+                  <form class="contact_us_form row" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+                     <div class="form-group col-lg-12">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                     </div>
+                     <div class="form-group col-lg-12">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                     </div>
+                     <div class="form-group col-lg-12">
+                        <input type="text" class="form-control" id="subject" name="subject" placeholder="Website">
+                     </div>
+                     <div class="form-group col-lg-12">
+                        <textarea class="form-control" name="message" id="message" rows="1" placeholder="Message"></textarea>
+                     </div>
+                     <div class="form-group col-md-12">
+                        <button type="submit" value="submit" class="btn submit_btn2 form-control">Send Messages</button>
+                     </div>
+                  </form>
+               </div>
             </div>
-            <!-- <div class="contact_d_list_item">
-               <p>Open hours: 8.00-18.00 Mon-Fri <br />Sunday: Closed</p>
-               </div> -->
          </div>
       </div>
    </div>
-</div>
+</section>
