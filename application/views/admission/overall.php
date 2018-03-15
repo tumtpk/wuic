38317ae45bcbb972813ce8eff7ca65a1778bda52
tumtpk <pk.prateep@gmail.com>
2018-03-15 12:20:19 +0700

    add bladecrum

--- application/views/admission/overall.php
+++ application/views/admission/overall.php
@@ -1,6 +1,17 @@
 <div class="container">
 <!--================Static Area =================-->
 <section class="static_area">
+	<p class="pblade">
+			<?php 
+            if(isset($breadcrumb)){
+                if(sizeof($breadcrumb) > 0){
+                     foreach ($breadcrumb as $array){
+                         echo '<a href="'.$array['url'].'"><i class="'.$array['icon'].'" aria-hidden="true"></i> '.$array['page'].'</a> ';
+                     }
+                }
+             }
+         ?>
+	</p>
 <div class="container">
 <div class="static_inner">
 <div class="row">
--- application/views/page/header_page.php
+++ application/views/page/header_page.php
@@ -2,17 +2,6 @@
    <div class="container">
       <div class="banner_text_inner">
          <h4><span style="font-family:'Bodoni Ornaments';"><span style="color: #8551a1;text-shadow: rgba(255, 255, 255, 0.85) 3px -2px;">We build </span><span style="color: #f27027;text-shadow: rgba(255, 255, 255, 0.85) 3px -2px;">“Next Generation<br /> Global leaders”</span></span></h4>
-         <?php 
-            if(isset($breadcrumb)){
-                if(sizeof($breadcrumb) > 0){
-                     echo " <ul>";
-                     foreach ($breadcrumb as $array){
-                         echo '<li><a href="'.$array['url'].'"><i class="'.$array['icon'].'" aria-hidden="true"></i>'.$array['page'].'</a></li>';
-                     }
-                     echo " </ul>";
-                }
-             }
-         ?>
       </div>
    </div>
 </section>
